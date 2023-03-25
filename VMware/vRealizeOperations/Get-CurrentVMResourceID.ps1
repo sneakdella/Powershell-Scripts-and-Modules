fc2898b4a8857c27647aea698f0afab5290fef48
Yuhasz <yuhas.jake@gmail.com>
2023-03-25 12:08:15 -0400

    Update Get-CurrentVMResourceID.ps1
    Completed the script. This is version 1.0, and it only works with vRealize Operations 8.10

--- VMware/vRealizeOperations/Get-CurrentVMResourceID.ps1
+++ VMware/vRealizeOperations/Get-CurrentVMResourceID.ps1
@@ -72,7 +72,11 @@ function Search-ForSelf {
     $SerialNumber = $SerialNumber.TrimStart("VMware-")
     $SerialNumber = $SerialNumber.Replace(" ","")
     $SerialNumber = $SerialNumber.Replace("-","")
-    $SerialNumber
+
+    # Debug
+    If ($FunctionDebug -eq $true) {
+        Write-Host $SerialNumber
+    }
 
     # Gather resource IDs for the Virtual Machines, put them into an Array list
     $resourceIds = [System.Collections.ArrayList]::new()
@@ -102,24 +106,98 @@ function Search-ForSelf {
 
     $response = Invoke-RestMethod "https://$RemoteCollector/suite-api/api/resources/properties/latest/query?_no_links=true" -Method 'POST' -Headers $Headers -Body $jsonBody -SkipCertificateCheck
 
+    $resourceId = ""
     ForEach ($VM in $response.values) {
         
         # Annoying debug, just uncomment if you are going to use.
         #Write-Host $VM."property-contents"."property-content" $VM."property-contents"."property-content"."statKey" $VM."property-contents"."property-content"."values"
 
         $VM_serialFromvROps = ($VM."property-contents"."property-content"."values").replace("-","")
-
+        
         If ($VM."property-contents"."property-content"."statKey" -eq "summary|smbiosUUID") {
             If ($VM_serialFromvROps -eq $SerialNumber) {
-                Write-Host "Matched this current host using SMBIOS UUID"
-                Write-Host "Current VM Serial Number: $SerialNumber"
+                If ($FunctionDebug -eq $true){
+                    Write-Host "function Search-ForSelf DEBUG: Matched this current host using SMBIOS UUID"
+                    Write-Host "function Search-ForSelf DEBUG: Current VM Serial Number: $SerialNumber"
+                    Write-Host "function Search-ForSelf DEBUG:`$VM.resourceId:$($VM.resourceId)"
+                }
                 Write-Host "Matched with Virtual Machine object resource:"$VM.resourceId"that has SMBIOS UUID of:"$VM_serialFromvROps
-                return $VM.resourceId
+                $resourceId = $VM.resourceId
+                break
             }
         }
+    }
+
+    If ($FunctionDebug -eq $true){
+        Write-Host "function Search-ForSelf DEBUG:`$resourceID:$resourceId"
+    }
+    
+    return $resourceId
+}
+
+function Get-AutoServices {
+    
+    param (
+        [Parameter(Mandatory=$false)]$FunctionDebug=$false   
+    )
+
+    $ErrorActionPreference = 'SilentlyContinue'
+
+    $AllAutoServices = Get-Service | Select-Object -Property Name,DisplayName,Status,StartType | Where-Object {$_.Status -eq "Running" -and $_.StartType -eq "Automatic"}
+
+    $AutomaticServicesNoDelay = [System.Collections.ArrayList]::new()
+
+    # Remove delayed start
+    ForEach ($service in $AllAutoServices) {
+        If (Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\$($service.Name)" | Where-Object {$_.Start -eq 2 -and $_.DelayedAutoStart -ne 1}) {
+            [void]$AutomaticServicesNoDelay.Add($service.Name)
+        }
+    }
+
+    # Debug
+    If ($FunctionDebug -eq $true) {
+        Write-Host "function Get-AutoServices DEBUG: Count of All automatic serivces (delayed too): "$AllAutoServices.Count
+        Write-Host "function Get-AutoServices DEBUG: Count of Automatic Services only (no delayed): "$AutomaticServicesNoDelay.Count
+    }
+    
+
+    $FinalAutomaticServices = Get-Service -Name $AutomaticServicesNoDelay | Select-Object -Property Name,DisplayName,Status
+    return $FinalAutomaticServices
+}
+
+function Invoke-NewServices {
+    param (
+        [Parameter(Mandatory=$true)]$RemoteCollector,
+        [Parameter(Mandatory=$true)]$AutomaticServices,
+        [Parameter(Mandatory=$true)]$CurrentVM,
+        [Parameter(Mandatory=$true)]$Headers,
+        [Parameter(Mandatory=$false)]$FunctionDebug=$false
+    )
+
+    #$FirstAutoService = $AutomaticServices[0]
+    #$jsonBody = "{ `"services`": [ { `"serviceName`": `"serviceavailability`", `"configurations`": [ {`"configName`": `"$($FirstAutoService.DisplayName)`",`"isActivated`": true,`"parameters`":[{`"key`":`"FILTER_VALUE`",`"value`":`"$($FirstAutoService.Name)`"}]}]}]}"
+    
+    
+    ForEach ($service in $AutomaticServices){
+
+        $jsonBody = "{ `"services`": [ { `"serviceName`": `"serviceavailability`", `"configurations`": [ {`"configName`": `"$($service.DisplayName)`",`"isActivated`": true,`"parameters`":[{`"key`":`"FILTER_VALUE`",`"value`":`"$($service.Name)`"}]}]}]}"
+
+        If ($FunctionDebug -eq $true){
+            Write-Host "function Invoke-NewServices DEBUG: `$jsonBody contents: "$jsonBody
+        }
+
+        try {
+            Invoke-RestMethod "https://$RemoteCollector/suite-api/api/applications/agents/$CurrentVM/services?_no_links=true" -Method 'POST' -Headers $Headers -Body $jsonBody -SkipCertificateCheck    
+        }
+        catch {
+            Write-Host "function Invoke-NewServices ERROR: Service: $($service.DisplayName) with exec name $($service.Name) either exists already or Invoke-RestMethod failed due to JSON formatting."
+        }
+        
     }
 }
 
 Get-vROpsAccessToken -RemoteCollector $RemoteCollector -Credential $Credential -AuthSource $AuthSource
 $VirtualMachines = Get-VirtualMachines -RemoteCollector $RemoteCollector -Headers $Headers
-Search-ForSelf -RemoteCollector $RemoteCollector -VirtualMachines $VirtualMachines -Headers $Headers
+$CurrentVM = Search-ForSelf -RemoteCollector $RemoteCollector -VirtualMachines $VirtualMachines -Headers $Headers
+$AutomaticServices = Get-AutoServices
+Invoke-NewServices -RemoteCollector $RemoteCollector -AutomaticServices $AutomaticServices -CurrentVM $CurrentVM -Headers $Headers
